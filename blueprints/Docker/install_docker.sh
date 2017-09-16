7b2c17a2b3ac2e2d590c5c086d9f7087c0290b88
nick <nconder@users.noreply.github.com>
2017-09-15 21:36:17 -0500

    Update install_docker.sh

--- blueprints/Docker/install_docker.sh
+++ blueprints/Docker/install_docker.sh
@@ -20,13 +20,14 @@ if [ $is_ubuntu -eq 0 ]; then
     apt-cache policy docker-ce
     apt-get install -y docker-ce
     # Hello Docker
-    service docker status
+    #service docker status
     # Add current user to Docker group to avoid pass on sudo
     usermod -aG docker ${USER}
     su - ${USER}
     id -nG
     # Add additional user to Docker group
     usermod -aG docker ocadmin
+    reboot now
 else
         echo "Sorry this script is for Ubuntu only." 
 fi
